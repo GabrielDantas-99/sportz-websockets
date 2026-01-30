1a21b315f1f5b06c19e2b4975563e83000db88c5
Gabriel Dantas <gabrieldantaslp@gmail.com>
2026-01-30 12:36:12 -0300

    Implement matchs POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
